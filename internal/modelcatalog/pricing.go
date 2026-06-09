63381483a9e40c3d49bb962036cfa5f01d92f55b
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2026-06-09 11:38:31 -0300

    phase-01/task-1: remove obsolete api/, internal/, and root e2e tests

--- internal/modelcatalog/pricing.go
+++
@@ -1,7 +0,0 @@
-package modelcatalog
-
-type Pricing struct {
-	InputPerMillionUSD       float64
-	CachedInputPerMillionUSD float64
-	OutputPerMillionUSD      float64
-}
--- internal/store/errors.go
+++
@@ -1,6 +0,0 @@
-package store
-
-import "errors"
-
-var ErrNotFound = errors.New("store: not found")
-var ErrDuplicateName = errors.New("store: duplicate name")
